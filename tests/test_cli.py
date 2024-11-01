2b3b1e39aec3dd1e8fc88b685b429df7d2928527
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-11-01 15:31:47 +0100

    Add one cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,40 +1,40 @@
-# from pathlib import Path
+from pathlib import Path
 
-# from mpi4py import MPI
+from mpi4py import MPI
 
-# import pytest
-# from click.testing import CliRunner
+import pytest
+from click.testing import CliRunner
 
-# from cardiac_geometries import Geometry, cli
+from cardiac_geometries import Geometry, cli
 
 
-# @pytest.mark.parametrize(
-#     "script",
-#     [
-#         cli.slab,
-#         cli.lv_ellipsoid,
-#     ],
-#     ids=["slab", "lv_ellipsoid"],
-# )
-# @pytest.mark.parametrize("fiber_space", [None, "P_1", "P_2", "Quadrature_2", "DG_1"])
-# def test_script(fiber_space, script, tmp_path: Path):
-#     runner = CliRunner()
+@pytest.mark.parametrize(
+    "script",
+    [
+        cli.slab,
+        cli.lv_ellipsoid,
+    ],
+    ids=["slab", "lv_ellipsoid"],
+)
+@pytest.mark.parametrize("fiber_space", [None, "P_1", "P_2", "Quadrature_2", "DG_1"])
+def test_script(fiber_space, script, tmp_path: Path):
+    runner = CliRunner()
 
-#     comm = MPI.COMM_WORLD
-#     path = comm.bcast(tmp_path, root=0)
+    comm = MPI.COMM_WORLD
+    path = comm.bcast(tmp_path, root=0)
 
-#     args = [path.as_posix()]
-#     if fiber_space is not None:
-#         args.extend(["--create-fibers", "--fiber-space", fiber_space])
+    args = [path.as_posix()]
+    if fiber_space is not None:
+        args.extend(["--create-fibers", "--fiber-space", fiber_space])
 
-#     res = runner.invoke(script, args)
-#     assert res.exit_code == 0
-#     assert path.is_dir()
-#     geo = Geometry.from_folder(comm=comm, folder=path)
-#     assert geo.mesh.geometry.dim == 3
+    res = runner.invoke(script, args)
+    assert res.exit_code == 0
+    assert path.is_dir()
+    geo = Geometry.from_folder(comm=comm, folder=path)
+    assert geo.mesh.geometry.dim == 3
 
-#     if fiber_space is not None:
-#         assert geo.f0 is not None
+    if fiber_space is not None:
+        assert geo.f0 is not None
 
 
 # @pytest.mark.parametrize(
