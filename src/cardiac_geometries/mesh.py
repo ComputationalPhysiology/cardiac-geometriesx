63cf620f45429dab75bdc1674c4b258c848c078b
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-11-01 19:40:15 +0100

    from folder

--- src/cardiac_geometries/mesh.py
+++ src/cardiac_geometries/mesh.py
@@ -664,7 +664,6 @@ def slab(
 
     geometry = utils.gmsh2dolfin(comm=comm, msh_file=mesh_name)
 
-    exit()
     if comm.rank == 0:
         with open(outdir / "markers.json", "w") as f:
             json.dump(geometry.markers, f, default=utils.json_serial)
@@ -683,7 +682,7 @@ def slab(
         )
 
     geo = Geometry.from_folder(comm=comm, folder=outdir)
-
+    exit()
     return geo
 
 
