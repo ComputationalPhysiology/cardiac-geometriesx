36ae93126faebbab4cacadbb25238cf1dc2d1e6b
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-12-03 15:59:58 +0100

    Update ukb-atlas demo

--- demos/ukb_atlas.py
+++ demos/ukb_atlas.py
@@ -6,7 +6,6 @@
 # However, if you want to use these meshes for cardiac simulations you probably need to generate fiber orientations using [`fenicsx-ldrb](https://finsberg.github.io/fenicsx-ldrb/README.html) as well.
 # Here we will show how to do this programmatically from python
 
-# +
 from pathlib import Path
 import numpy as np
 import json
@@ -18,16 +17,23 @@ import cardiac_geometries as cg
 import ukb.cli
 
 # Select the mode you want to use (-1 will you the mean mode)
+
 mode = -1
+
 # Standard deviation of the mode
 std = 1.5
+
 # Choose output directory
+
 outdir = Path("ukb_mesh")
 outdir.mkdir(exist_ok=True)
 subdir = outdir / f"mode_{int(mode)}"
+
 # Characteristic length of the mesh (smaller values will give finer meshes)
 char_length = 10.0
+
 # Generate mesh
+
 ukb.cli.main(
     [
         str(outdir),
@@ -50,6 +56,8 @@ mesh_name = subdir / "ED.msh"
 comm = MPI.COMM_WORLD
 geometry = cg.utils.gmsh2dolfin(comm=comm, msh_file=mesh_name)
 
+# Now we can plot the mesh
+
 pyvista.start_xvfb()
 vtk_mesh = dolfinx.plot.vtk_mesh(geometry.mesh, geometry.mesh.topology.dim)
 grid = pyvista.UnstructuredGrid(*vtk_mesh)
@@ -61,13 +69,15 @@ if not pyvista.OFF_SCREEN:
 else:
     figure = plotter.screenshot("ukb_mesh.png")
 
-# Save the markers to json
+# We als save the markers to json
+
 print(geometry.markers)
 if comm.rank == 0:
     (subdir / "markers.json").write_text(json.dumps(geometry.markers, default=cg.utils.json_serial))
 comm.barrier()
 
-# Plot the facet tags
+# And plot the facet tags
+
 assert geometry.ffun is not None
 vtk_bmesh = dolfinx.plot.vtk_mesh(geometry.mesh, geometry.ffun.dim, geometry.ffun.indices)
 bgrid = pyvista.UnstructuredGrid(*vtk_bmesh)
@@ -80,10 +90,8 @@ if not pyvista.OFF_SCREEN:
 else:
     figure = p.screenshot("facet_tags_ukb.png")
 
+# Now we will generate fiber orientations using fenicsx-ldrb. To do this we first combine all the outflow tracts into one entity, i.e the `BASE`
 
-# Generate fiber orientations
-
-# We combine all the outflow tracts into one entity, i.e the BASE
 entities = [
     geometry.ffun.find(1),  # LV
     geometry.ffun.find(2),  # RV
@@ -99,21 +107,22 @@ entities = [
 ]
 values = [np.full(e.shape, i + 1, dtype=np.int32) for i, e in enumerate(entities)]
 
-# Create the mesh tags
+# and create new mesh tags
 
 ffun = dolfinx.mesh.meshtags(geometry.mesh, 2, np.hstack(entities), np.hstack(values))
 
-# And plot it
+# and plot the new tags
 
-bgrid.cell_data["Facet tags"] = ffun.values
-bgrid.set_active_scalars("Facet tags")
+vtk_bmesh_new = dolfinx.plot.vtk_mesh(geometry.mesh, ffun.dim, ffun.indices)
+bgrid_new = pyvista.UnstructuredGrid(*vtk_bmesh_new)
+bgrid_new.cell_data["Facet tags"] = ffun.values
+bgrid_new.set_active_scalars("Facet tags")
 p = pyvista.Plotter(window_size=[800, 800])
-p.add_mesh(bgrid, show_edges=True)
+p.add_mesh(bgrid_new, show_edges=True)
 if not pyvista.OFF_SCREEN:
     p.show()
 else:
     figure = p.screenshot("new_facet_tags_ukb.png")
-
 
 # Let us also save the markers so that we can inspect them later
 
@@ -122,6 +131,7 @@ with dolfinx.io.XDMFFile(geometry.mesh.comm, subdir / "new_ffun.xdmf", "w") as x
     xdmf.write_meshtags(ffun, geometry.mesh.geometry)
 
 # Now let ut combine the markers as well so that they align with the markers needed in fenicsx-ldrb
+
 markers = {}
 keymap = {"LV": "lv", "RV": "rv", "EPI": "epi"}
 for k, v in geometry.markers.items():
@@ -129,6 +139,7 @@ for k, v in geometry.markers.items():
 markers["base"] = [geometry.markers[m][0] for m in ["MV", "PV", "TV", "AV"]]
 
 # And finally generate the fibers
+
 fiber_space = "P_1"
 fiber_angle_endo = 60
 fiber_angle_epi = -60
@@ -142,12 +153,14 @@ system = ldrb.dolfinx_ldrb(
     beta_epi_lv=0,
     fiber_space=fiber_space,
 )
-# Save it to
+
+# and save them
+
 cg.fibers.utils.save_microstructure(
     mesh=geometry.mesh, functions=(system.f0, system.s0, system.n0), outdir=subdir
 )
 
-# Let us plot the fibers
+# Let us also plot the fibers
 
 topology_f0, cell_types_f0, geometry_f0 = dolfinx.plot.vtk_mesh(system.f0.function_space)
 values = np.zeros((geometry_f0.shape[0], 3), dtype=np.float64)
@@ -164,7 +177,6 @@ if not pyvista.OFF_SCREEN:
     plotter.show()
 else:
     fig_as_array = plotter.screenshot("fiber_ukb.png")
-
 
 # You should now be able to load the geometry and the fibers using the following code
 
